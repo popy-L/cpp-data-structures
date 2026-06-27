d6aed1f261a5f5bc6604b186585b64e6a536b328
-popify- <p1917747297@gmail.com>
2026-06-27 12:26:30 +0800

    Create 尾插法.cpp

---
+++ 第二章 线性表/基础知识/尾插法.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node*next;
+};
+
+void InsertHead(Node*&head,int value) {
+    Node* p=new Node;
+    p->data=value;
+    p->next=head;
+    head=p;
+}
+int main() {
+   Node* head=nullptr;
+   for(int i=0;i<3;i++){
+    InsertHead(head,i);
+   }
+Node* p=head;
+while(p!=nullptr){
+    cout<<p->data<<endl;
+    p=p->next;
+}
+tem->next = p
+};
+
