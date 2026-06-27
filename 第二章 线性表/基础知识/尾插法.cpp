173e1b0e8702a020fcdf34e0730a8ea010cf20ba
-popify- <p1917747297@gmail.com>
2026-06-27 12:29:48 +0800

    Update 尾插法.cpp

--- 第二章 线性表/基础知识/尾插法.cpp
+++ 第二章 线性表/基础知识/尾插法.cpp
@@ -1,15 +1,13 @@
 void InsertTail(Node*& head, int value) {
     Node* p = new Node;
     p->data = value;
-    p->next = nullptr;   // 新节点是尾巴，所以 next 必须指向 nullptr！
-
-    // TODO: 补全下面代码
+    p->next = nullptr;   //新节点是尾巴，所以 next 必须指向 nullptr！
     
-    // 1. 如果链表为空，直接让 head = p
+    //如果链表为空，直接让 head = p
     if(head==nullptr){
         head=p;
     }
-    // 2. 否则，找到最后一个节点，把 p 挂上去
+    //否则，找到最后一个节点，把 p 挂上去
     else{
         Node* tem=head;
         while(tem->next!=nullptr){
