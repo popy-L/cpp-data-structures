3ce8bd08042b931fb8d2d3880cd8b021c7bc8084
-popify- <p1917747297@gmail.com>
2026-06-27 11:05:53 +0800

    Update 头插法.cpp

--- 第二章 线性表/基础知识/头插法.cpp
+++ 第二章 线性表/基础知识/头插法.cpp
@@ -5,16 +5,20 @@ struct Node {
     Node*next;
 };
 
-void claude(Node*&head,int value) {
-    Node*p=new Node;
+void InsertHead(Node*&head,int value) {
+    Node* p=new Node;
     p->data=value;
     p->next=head;
     head=p;
 }
 int main() {
-    Node*head=nullptr;
-    int a;
-    claude(head,1);
-    cout<<head->data<<endl;
+   Node* head=nullptr;
+   for(int i=0;i<3;i++){
+    InsertHead(head,i);
+   }
+Node* p=head;
+while(p!=nullptr){
+    cout<<p->data<<endl;
+    p=p->next;
+   }
 };
-
